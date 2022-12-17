5dfdca1f429a8f136065cf790bc39002ff92a15a
AmanullahNasir <AmanullahNasir@SS-AMANULLAH>
2022-12-17 16:35:16 +0500

    Add project files.

---
+++ TWCTransport/Model/TRReqModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TWCTransport.Model
+{
+    public class TRReqModel
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
